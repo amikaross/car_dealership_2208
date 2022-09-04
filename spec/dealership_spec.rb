a78a2161c73962d7a0fc3d4d28b12df8ae2bc87a
Amanda Ross <amikaross@gmail.com>
2022-09-04 13:07:57 -0600

    test: add tests to dealership_spec for iteration 3

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Dealership do
     @dealership = Dealership.new('Acme Auto', '123 Main Street')
     @car_1 = Car.new('Ford Mustang', 1500, 36)
     @car_2 = Car.new('Toyota Prius', 1000, 48)
+    @car_3 = Car.new('Toyota Tercel', 500, 48)
+    @car_4 = Car.new('Chevrolet Bronco', 1250, 24)
   end
 
   describe '#initialize' do 
@@ -15,6 +17,15 @@ RSpec.describe Dealership do
 
     it 'has an empty inventory by default' do 
       expect(@dealership.inventory).to eq []
+    end
+
+    it 'has a readable "details" attribute' do 
+      @dealership.add_car(@car_1)
+      @dealership.add_car(@car_2)
+      @dealership.add_car(@car_3)
+      @dealership.add_car(@car_4)
+
+      expect(@dealership.details).to eq {'total_value' => 156000, 'address' => '123 Main Street'}
     end
   end
 
@@ -34,6 +45,41 @@ RSpec.describe Dealership do
       @dealership.add_car(@car_2)
 
       expect(@dealership.inventory).to eq [@car_1, @car_2]
+    end
+  end
+
+  describe '#has_inventory?' do 
+    it 'returns true/false depending on whether or not inventory is empty' do 
+      expect(@dealership.has_inventory?).to eq false 
+      @dealership.add_car(@car_1)
+      @dealership.add_car(@car_2)
+      @dealership.add_car(@car_3)
+      @dealership.add_car(@car_4)
+
+      expect(@dealership.has_inventory?).to eq true 
+    end
+  end
+
+  describe '#cars_by_make' do 
+    it 'returns an array that includes cars which match the given make' do 
+      @dealership.add_car(@car_1)
+      @dealership.add_car(@car_2)
+      @dealership.add_car(@car_3)
+      @dealership.add_car(@car_4)
+
+      expect(@dealership.cars_by_make('Toyota')).to eq [@car_2, @car_3]
+      expect(@dealership.cars_by_make('Ford')).to eq [@car_1]
+    end
+  end
+
+  describe '#total_value' do 
+    it 'returns the total value of all the cars in the inventory' do 
+      @dealership.add_car(@car_1)
+      @dealership.add_car(@car_2)
+      @dealership.add_car(@car_3)
+      @dealership.add_car(@car_4)
+
+      expect(@dealership.total_value).to eq 156000
     end
   end
 end
