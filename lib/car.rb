54b1000cbe268045fa24957d6d928b1b84ff985f
Amanda Ross <amikaross@gmail.com>
2022-09-04 12:32:15 -0600

    create Car class and add color and painted! tests

---
+++ lib/car.rb
@@ -0,0 +1,17 @@
+class Car
+  attr_reader :make,
+              :model,
+              :monthly_payment,
+              :loan_length,
+              :total_cost,
+              :color
+
+  def initialize(make_and_model, monthly_payment, loan_length)
+    @make = make_and_model.split[0]
+    @model = make_and_model.split[1] 
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+    @total_cost = monthly_payment * loan_length
+    @color = nil 
+  end
+end
