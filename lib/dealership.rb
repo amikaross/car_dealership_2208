38d086764d6b70993f555d7005a63ff2e71f2705
Amanda Ross <amikaross@gmail.com>
2022-09-04 13:20:00 -0600

    feature: add total_value method to dealership class

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -24,4 +24,12 @@ class Dealership
   def cars_by_make(make)
     @inventory.select { |car| car.make == make }
   end
+
+  def total_value
+    total_value = 0
+    @inventory.each do |car|
+      total_value += car.total_cost
+    end
+    total_value
+  end
 end
